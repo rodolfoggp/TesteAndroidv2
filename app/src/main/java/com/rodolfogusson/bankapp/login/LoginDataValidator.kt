a6c7d1799c3dd4f98bba1ed7df85c6e65e90f1c2
Rodolfo Gusson <rodolfoggp@gmail.com>
2019-02-08 15:33:43 -0200

    LoginDataValidator implementation finished

--- app/src/main/java/com/rodolfogusson/bankapp/login/LoginDataValidator.kt
+++ app/src/main/java/com/rodolfogusson/bankapp/login/LoginDataValidator.kt
@@ -7,19 +7,19 @@ interface Validator {
 class LoginDataValidator : Validator{
 
     override fun validate(user: User): Boolean {
-        return validateLogin(user.login) //&& validatePassword(user.password)
+        return validateLogin(user.login) && validatePassword(user.password)
     }
 
-//    private fun validatePassword(password: String): Boolean {
-//        return true
-//    }
-
     private fun validateLogin(login: String): Boolean {
+        return validateCPF(login) || validateEmail(login)
+    }
+
+    private fun validateCPF(login: String): Boolean {
         //If login doesn't contain only numbers, dots and dash, it's invalid
         if (!login.matches("[-0-9.]+".toRegex())) return false
 
         //If there aren't 11 numbers in the string, it's invalid
-        val numericLogin = login.replace("[^0-9]".toRegex(),"")
+        val numericLogin = login.replace("[^0-9]".toRegex(), "")
         if (numericLogin.length != 11) return false
 
         //If there are only repeated numbers, it's invalid
@@ -28,7 +28,7 @@ class LoginDataValidator : Validator{
         //Validate the first verification digit
         var sum1 = 0
         for (i in 0..8) {
-            sum1 += (10-i) * numericLogin[i].toString().toInt()
+            sum1 += (10 - i) * numericLogin[i].toString().toInt()
         }
         var mod1 = (sum1 * 10) % 11
         if (mod1 == 10) mod1 = 0
@@ -37,12 +37,34 @@ class LoginDataValidator : Validator{
         //Validate the second verification digit
         var sum2 = 0
         for (i in 0..9) {
-            sum2 += (11-i) * numericLogin[i].toString().toInt()
+            sum2 += (11 - i) * numericLogin[i].toString().toInt()
         }
         var mod2 = (sum2 * 10) % 11
         if (mod2 == 10) mod2 = 0
         if (mod2 != numericLogin[10].toString().toInt()) return false
 
         return true
+    }
+
+    private fun validateEmail(login: String): Boolean {
+        //Email regex for Java, picked from emailregex.com
+        val emailRegex = ("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+" +
+                "(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*" +
+                "|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]" +
+                "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
+                "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+" +
+                "[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" +
+                "|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
+                "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?" +
+                "|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]" +
+                "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+                .trimMargin()
+                .toRegex()
+        return login.matches(emailRegex)
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        val passwordRegex = "^(?=.*[A-Z])(?=.*[a-zA-Z0-9])(?=.*[^a-zA-Z0-9]).+\$".toRegex()
+        return passwordRegex.matches(password)
     }
 }
--- app/src/test/java/com/rodolfogusson/bankapp/login/LoginDataValidatorUnitTest.kt
+++ app/src/test/java/com/rodolfogusson/bankapp/login/LoginDataValidatorUnitTest.kt
@@ -19,7 +19,7 @@ class LoginDataValidatorUnitTest {
     }
 
     @Test
-    fun `when a valid CPF is put to validation, it is valid`() {
+    fun `a valid CPF should be valid`() {
         //WHEN
         val valid = validator.validate(User(validCPF, validPassword))
 
@@ -28,7 +28,7 @@ class LoginDataValidatorUnitTest {
     }
 
     @Test
-    fun `when a valid CPF, separated by dots and dash in the correct format, is validated, it is valid`() {
+    fun `a CPF, separated by dots and dash in the correct format, should be valid`() {
         //GIVEN
         val validCPFDotsDash = "529.982.247-25"
 
@@ -40,7 +40,7 @@ class LoginDataValidatorUnitTest {
     }
 
     @Test
-    fun `when CPF with only numbers does not contain 11 digits, it is invalid`() {
+    fun `a CPF with only numbers that does not contain 11 digits, should be invalid`() {
         //GIVEN
         val cpf12 = "123456789123"
         val cpf10 = "1234567890"
@@ -63,7 +63,7 @@ class LoginDataValidatorUnitTest {
 
 
     @Test
-    fun `when CPF doesn't contain only numbers, dots and dashes, it is invalid`() {
+    fun `a CPF that doesn't contain only numbers, dots and dashes should be invalid`() {
         //GIVEN
         val invalidCPF1 = "529.982.A247-25"
         val invalidCPF2 = "529.982.A47-25"
@@ -78,12 +78,177 @@ class LoginDataValidatorUnitTest {
     }
 
     @Test
-    fun `when CPF contains only repeated numbers, it is invalid`(){
+    fun `a CPF containing only repeated numbers should be invalid`(){
         //GIVEN
         val cpf = "11111111111"
 
         //WHEN
         val valid = validator.validate(User(cpf, validPassword))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `a valid email should be valid`() {
+        //GIVEN
+        val validEmail = "email@domain.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertTrue(valid)
+    }
+
+    @Test
+    fun `an email with domain and subdomain should be valid`() {
+        //GIVEN
+        val validEmail = "email@subdomain.domain.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertTrue(valid)
+    }
+
+    @Test
+    fun `an email with dash should be valid`() {
+        //GIVEN
+        val validEmail = "firstname-lastname@domain.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertTrue(valid)
+    }
+
+    @Test
+    fun `an email with dot should be valid`() {
+        //GIVEN
+        val validEmail = "firstname.lastname@domain.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertTrue(valid)
+    }
+
+    @Test
+    fun `an email with IP inside brackets should be valid`() {
+        //GIVEN
+        val validEmail = "email@[123.123.123.123]"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertTrue(valid)
+    }
+
+    @Test
+    fun `an email missing @ sign and domain should be invalid`() {
+        //GIVEN
+        val validEmail = "plainaddress"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `an email with garbage before dot com should be invalid`() {
+        //GIVEN
+        val validEmail = "#@%^%#\\$@#\\$@#.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `an email missing username should be invalid`() {
+        //GIVEN
+        val validEmail = "@domain.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `an email missing @ sign should be invalid`() {
+        //GIVEN
+        val validEmail = "email.domain.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `an email with multiple dots should be invalid`() {
+        //GIVEN
+        val validEmail = "email..email@domain.com"
+
+        //WHEN
+        val valid = validator.validate(User(validEmail, validPassword))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `a password with at least 1 capital letter, 1 special character and 1 alphanumeric character should be valid`() {
+        //WHEN
+        val valid = validator.validate(User(validCPF, validPassword))
+
+        //THEN
+        assertTrue(valid)
+    }
+
+    @Test
+    fun `a password without a capital letter should be invalid`() {
+        //GIVEN
+        val password = "a&a"
+
+        //WHEN
+        val valid = validator.validate(User(validCPF, password))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `a password without a special character should be invalid`() {
+        //GIVEN
+        val password = "abc123ABC"
+
+        //WHEN
+        val valid = validator.validate(User(validCPF, password))
+
+        //THEN
+        assertFalse(valid)
+    }
+
+    @Test
+    fun `a password without an alphanumeric character should be invalid`() {
+        //GIVEN
+        val password = "&#$&$%"
+
+        //WHEN
+        val valid = validator.validate(User(validCPF, password))
 
         //THEN
         assertFalse(valid)
